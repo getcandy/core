04673b53cd97c2e457334736ae0a84fb55f862f2
Alec Ritson <hello@itsalec.co.uk>
2021-12-22 14:50:06 +0000

    Update installer

--- src/Console/InstallGetCandy.php
+++ src/Console/InstallGetCandy.php
@@ -2,7 +2,20 @@
 
 namespace GetCandy\Console;
 
+use GetCandy\Models\Attribute;
+use GetCandy\FieldTypes\TranslatedText;
+use GetCandy\Hub\Models\Staff;
+use GetCandy\Models\AttributeGroup;
+use GetCandy\Models\Channel;
+use GetCandy\Models\Collection;
+use GetCandy\Models\Country;
+use GetCandy\Models\Currency;
+use GetCandy\Models\CustomerGroup;
+use GetCandy\Models\Language;
+use GetCandy\Models\ProductType;
+use GetCandy\Models\TaxClass;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 
 class InstallGetCandy extends Command
@@ -28,22 +41,218 @@ class InstallGetCandy extends Command
      */
     public function handle(): void
     {
-        $this->info('Installing GetCandy...');
+        DB::transaction(function () {
+            $this->info('Installing GetCandy...');
 
-        $this->info('Publishing configuration...');
+            $this->info('Publishing configuration...');
 
-        if (! $this->configExists('getcandy')) {
-            $this->publishConfiguration();
-        } else {
-            if ($this->shouldOverwriteConfig()) {
-                $this->info('Overwriting configuration file...');
-                $this->publishConfiguration($force = true);
+            if (! $this->configExists('getcandy')) {
+                $this->publishConfiguration();
             } else {
-                $this->info('Existing configuration was not overwritten');
+                if ($this->shouldOverwriteConfig()) {
+                    $this->info('Overwriting configuration file...');
+                    $this->publishConfiguration($force = true);
+                } else {
+                    $this->info('Existing configuration was not overwritten');
+                }
             }
-        }
 
-        $this->info('GetCandy is now installed.');
+            $this->info('Publishing hub assets');
+
+            $this->publishResources();
+
+            if (! Country::count()) {
+                $this->info('Importing countries');
+                $this->call('getcandy:import:address-data');
+            }
+
+            if (! Channel::whereDefault(true)->exists()) {
+                $this->info('Setting up default channel');
+
+                Channel::create([
+                    'name' => 'Webstore',
+                    'handle' => 'webstore',
+                    'default' => true,
+                    'url' => 'localhost',
+                ]);
+            }
+
+            if (! Staff::whereAdmin(true)->exists()) {
+                $this->info('Create an admin user');
+
+                $firstname = $this->ask('Whats your first name?');
+                $lastname = $this->ask('Whats your last name?');
+                $email = $this->ask('Whats your email address?');
+                $password = $this->secret('Enter a password');
+
+                Staff::create([
+                    'firstname' => $firstname,
+                    'lastname' => $lastname,
+                    'email' => $email,
+                    'password' => bcrypt($password),
+                    'admin' => true,
+                ]);
+            }
+
+            if (! Language::count()) {
+                $this->info('Adding default language');
+
+                Language::create([
+                    'code' => 'en',
+                    'name' => 'English',
+                    'default' => true,
+                ]);
+            }
+
+            if (! Currency::whereDefault(true)->exists()) {
+                $this->info('Adding a default currency (USD)');
+
+                Currency::create([
+                    'code' => 'USD',
+                    'name' => 'US Dollar',
+                    'exchange_rate' => 1,
+                    'format' => '${value}',
+                    'decimal_point' => '.',
+                    'thousand_point' => ',',
+                    'decimal_places' => 2,
+                    'default' => true,
+                ]);
+            }
+
+            if (! CustomerGroup::whereDefault(true)->exists()) {
+                $this->info('Adding a default customer group.');
+
+                CustomerGroup::create([
+                    'name' => 'Retail',
+                    'handle' => 'retail',
+                    'default' => true,
+                ]);
+            }
+
+            if (! TaxClass::count()) {
+                $this->info('Adding a default tax class.');
+
+                TaxClass::create([
+                    'name' => 'Default Tax Class',
+                ]);
+            }
+
+            if (! Attribute::count()) {
+                $this->info('Setting up initial attributes');
+
+                $group = AttributeGroup::create([
+                    'attributable_type' => ProductType::class,
+                    'name' => collect([
+                        'en' => 'Details',
+                    ]),
+                    'handle' => 'details',
+                    'position' => 1,
+                ]);
+
+                $collectionGroup = AttributeGroup::create([
+                    'attributable_type' => Collection::class,
+                    'name' => collect([
+                        'en' => 'Details',
+                    ]),
+                    'handle' => 'collection_details',
+                    'position' => 1,
+                ]);
+
+                Attribute::create([
+                    'attribute_type' => ProductType::class,
+                    'attribute_group_id' => $group->id,
+                    'position' => 1,
+                    'name' => [
+                        'en' => 'Name',
+                    ],
+                    'handle' => 'name',
+                    'section' => 'main',
+                    'type' => TranslatedText::class,
+                    'required' => true,
+                    'default_value' => null,
+                    'configuration' => [
+                        'type' => 'text',
+                    ],
+                    'system' => true,
+                ]);
+
+                Attribute::create([
+                    'attribute_type' => Collection::class,
+                    'attribute_group_id' => $collectionGroup->id,
+                    'position' => 1,
+                    'name' => [
+                        'en' => 'Name',
+                    ],
+                    'handle' => 'name',
+                    'section' => 'main',
+                    'type' => TranslatedText::class,
+                    'required' => true,
+                    'default_value' => null,
+                    'configuration' => [
+                        'type' => 'text',
+                    ],
+                    'system' => true,
+                ]);
+
+                Attribute::create([
+                    'attribute_type' => ProductType::class,
+                    'attribute_group_id' => $group->id,
+                    'position' => 2,
+                    'name' => [
+                        'en' => 'Description',
+                    ],
+                    'handle' => 'description',
+                    'section' => 'main',
+                    'type' => TranslatedText::class,
+                    'required' => true,
+                    'default_value' => null,
+                    'configuration' => [
+                        'type' => 'richtext',
+                    ],
+                    'system' => true,
+                ]);
+
+                Attribute::create([
+                    'attribute_type' => Collection::class,
+                    'attribute_group_id' => $collectionGroup->id,
+                    'position' => 2,
+                    'name' => [
+                        'en' => 'Description',
+                    ],
+                    'handle' => 'description',
+                    'section' => 'main',
+                    'type' => TranslatedText::class,
+                    'required' => true,
+                    'default_value' => null,
+                    'configuration' => [
+                        'type' => 'richtext',
+                    ],
+                    'system' => true,
+                ]);
+            }
+
+            if (! ProductType::count()) {
+                $this->info('Adding a product type.');
+
+                $type = ProductType::create([
+                    'name' => 'Stock',
+                ]);
+
+                $type->mappedAttributes()->attach(
+                    Attribute::whereAttributeType(ProductType::class)->get()->pluck('id')
+                );
+            }
+
+            $this->info('GetCandy is now installed.');
+
+            if ($this->confirm('Would you like to show some love by starring the repo?')) {
+                $exec = PHP_OS_FAMILY === 'Windows' ? 'start' : 'open';
+
+                exec("{$exec} https://github.com/getcandy/getcandy");
+
+                $this->line("Thanks, you're awesome!");
+            }
+        });
     }
 
     /**
@@ -54,6 +263,9 @@ class InstallGetCandy extends Command
      */
     private function configExists($fileName): bool
     {
+        if (! File::isDirectory(config_path($fileName))) {
+            return false;
+        }
         return ! empty(File::allFiles(config_path($fileName)));
     }
 
@@ -88,5 +300,12 @@ class InstallGetCandy extends Command
         }
 
         $this->call('vendor:publish', $params);
+    }
+
+    private function publishResources()
+    {
+        $this->call('vendor:publish', [
+            '--tag' => 'getcandy:hub:public',
+        ]);
     }
 }
