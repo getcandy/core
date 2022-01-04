838c85db9cf5d90ea3c6292d5f9e37a4661c47f7
Alec Ritson <hello@itsalec.co.uk>
2022-01-04 11:27:07 +0000

    Add prefix to config

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -42,7 +42,7 @@ class Product extends BaseModel implements SpatieHasMedia
      */
     public function searchableAs()
     {
-        return 'products_'.app()->getLocale();
+        return config('scout.prefix').'products_'.app()->getLocale();
     }
 
     /**
