8eaff54573ba257b8fbda7f4d92f1ee043e15d51
Alec Ritson <hello@itsalec.co.uk>
2022-01-05 11:02:56 +0000

    Updates in line with customer screens in hub

--- src/Models/Customer.php
+++ src/Models/Customer.php
@@ -7,10 +7,11 @@ use GetCandy\Base\BaseModel;
 use GetCandy\Base\Traits\HasPersonalDetails;
 use GetCandy\Database\Factories\CustomerFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Scout\Searchable;
 
 class Customer extends BaseModel
 {
-    use HasFactory, HasPersonalDetails;
+    use HasFactory, HasPersonalDetails, Searchable;
 
     /**
      * Define the guarded attributes.
@@ -18,6 +19,13 @@ class Customer extends BaseModel
      * @var array
      */
     protected $guarded = [];
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $casts = [
+        'meta' => 'object'
+    ];
 
     /**
      * Return a new factory instance for the model.
@@ -30,26 +38,26 @@ class Customer extends BaseModel
     }
 
     /**
-     * Set the meta value.
-     *
-     * @param  array|null  $val
-     * @return void
-     */
-    public function setMetaAttribute(array $val = null)
-    {
-        if ($val) {
-            $this->attributes['meta'] = json_encode($val);
-        }
-    }
-
-    /**
-     * Get the meta value.
+     * Get the indexable data array for the model.
      *
      * @return array
      */
-    public function getMetaAttribute($value)
+    public function toSearchableArray()
     {
-        return json_decode($value);
+        $metaFields = config('getcandy-hub.customers.searchable_meta', []);
+
+        $data = [
+            'id' => $this->id,
+            'name' => $this->fullName,
+            'company_name' => $this->company_name,
+            'vat_no' => $this->vat_no,
+        ];
+
+        foreach ($metaFields as $field) {
+            $data[$field] = $this->meta?->{$field};
+        }
+
+        return $data;
     }
 
     /**
@@ -80,5 +88,15 @@ class Customer extends BaseModel
             config('auth.providers.users.model'),
             "{$prefix}customer_user"
         )->withTimestamps();
+    }
+
+    /**
+     * Return the addresses relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function addresses()
+    {
+        return $this->hasMany(Address::class);
     }
 }
