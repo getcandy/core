e5a67551c4e5df9270ce096f659745be66b373a8
GitHub <action@github.com>
2022-02-01 22:35:27 +0000

    Update roadmap link

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -119,9 +119,8 @@ class Product extends BaseModel implements SpatieHasMedia
     /**
      * Associate a product to another with a type.
      *
-     * @param mixed  $product
-     * @param string $type
-     *
+     * @param  mixed  $product
+     * @param  string  $type
      * @return void
      */
     public function associate($product, $type)
@@ -132,9 +131,8 @@ class Product extends BaseModel implements SpatieHasMedia
     /**
      * Dissociate a product to another with a type.
      *
-     * @param mixed  $product
-     * @param string $type
-     *
+     * @param  mixed  $product
+     * @param  string  $type
      * @return void
      */
     public function dissociate($product, $type = null)
