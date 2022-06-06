be85b271fd4950d06e7b78eaf215c3a2aa026dad
Glenn Jacobs <glennjacobs@users.noreply.github.com>
2022-06-06 09:49:41 +0000

    Apply fixes from StyleCI

--- src/Base/ShippingManifestInterface.php
+++ src/Base/ShippingManifestInterface.php
@@ -11,7 +11,8 @@ interface ShippingManifestInterface
     /**
      * Add a shipping option to the manifest.
      *
-     * @param  \GetCandy\DataTypes\ShippingOption  $shippingOption
+     * @param \GetCandy\DataTypes\ShippingOption $shippingOption
+     *
      * @return self
      */
     public function addOption(ShippingOption $shippingOption);
@@ -19,7 +20,8 @@ interface ShippingManifestInterface
     /**
      * Return available options for a given cart.
      *
-     * @param  \GetCandy\Models\Cart  $cart
+     * @param \GetCandy\Models\Cart $cart
+     *
      * @return \Illuminate\Support\Collection
      */
     public function getOptions(Cart $cart): Collection;
