0478233ad655949e59d25a86453179824342cd4d
GitHub <action@github.com>
2022-08-01 07:48:53 +0000

    Init (#444)

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -171,9 +171,8 @@ class Product extends BaseModel implements SpatieHasMedia
     /**
      * Associate a product to another with a type.
      *
-     * @param mixed  $product
-     * @param string $type
-     *
+     * @param  mixed  $product
+     * @param  string  $type
      * @return void
      */
     public function associate($product, $type)
@@ -184,9 +183,8 @@ class Product extends BaseModel implements SpatieHasMedia
     /**
      * Dissociate a product to another with a type.
      *
-     * @param mixed  $product
-     * @param string $type
-     *
+     * @param  mixed  $product
+     * @param  string  $type
      * @return void
      */
     public function dissociate($product, $type = null)
